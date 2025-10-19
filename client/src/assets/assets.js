51c5e7fce9430ead93e195f8426d3f1d920130be
rajeev-45 <dixitrajeev5202@gmail.com>
2025-10-19 12:57:11 +0530

    AddJObs and viewApplications page created

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import logo1 from "./logo1.svg"
 import search_icon from "./search_icon.svg";
 import company_icon from "./company_icon.svg";
 import microsoft_logo from "./microsoft_logo.svg";
@@ -36,6 +37,7 @@ import amazon_logo from './amazon_logo.png'
 
 export const assets = {
     logo,
+    logo1,
     search_icon,
     cross_icon,
     upload_area,
--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from 'react'
 import { useState } from 'react';
 import Quill from 'quill';
+import { JobCategories, JobLocations } from '../assets/assets';
 
 const AddJob = () => {
 
@@ -14,7 +15,7 @@ const AddJob = () => {
     const quillRef = useRef(null)
 
     useEffect(() => {
-  if (editorRef.current) {
+  if (!quillRef.current && editorRef.current) {
     quillRef.current = new Quill(editorRef.current, {
       theme: 'snow',
       placeholder: 'Write job description here...',
@@ -25,19 +26,50 @@ const AddJob = () => {
 
 
   return (
-    <form>
+    <form className='container p-4 flex flex-col w-full itmes-start gap-3'>
       
-      <div>
-        <p>Job Title</p>
+      <div className='w-full'>
+        <p className='mb-2'>Job Title</p>
         <input type="text" placeholder='Type here' 
-        onChange={e=>setTitle(e.target.value)} value={title} required/>
+        onChange={e=>setTitle(e.target.value)} value={title} required className='w-full max-w-lg px-3 py-2 border-2 border-gray-300 rounded'/>
       </div>
-      <div>
-        <p>Job Description</p>
+      <div className='w-full max-w-lg'>
+        <p className='my-2'>Job Description</p>
         <div ref={editorRef}>
 
         </div>
       </div>
+      <div className='flex flex-col sm:flex-row gap-2 w-full sm:gap-8'>
+        <div>
+          <p className='mb-2'>Job Category</p>
+          <select className='w-full px-3 py-2 border-2 border-gray-300 rounded' onChange={e => setCategory(e.target.value)}>
+            {JobCategories.map((category,index)=>(
+              <option key={index} value={category}>{category}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <p className='mb-2'>Job Location</p>
+          <select className='w-full px-3 py-2 border-2 border-gray-300 rounded' onChange={e => setLocation(e.target.value)}>
+            {JobLocations.map((location,index)=>(
+              <option key={index} value={location}>{location}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <p className='mb-2'>Job Level</p>
+          <select className='w-full px-3 py-2 border-2 border-gray-300 rounded' onChange={e => setLevel(e.target.value)}>
+            <option value="Beginner level">Beginner level</option>
+            <option value="Intermediate level">Intermediate level</option>
+            <option value="Senior level">Senior level</option>
+          </select>
+        </div>
+      </div>
+      <div>
+        <p className='mb-2'>Job Salary</p>
+        <input min={0} className='w-full px-3 py-2 border-2 border-gray-300 rounded sm:w-[120px]' onChange={e => setSalary(e.target.value)} type="Number" placeholder='2500' />
+      </div>
+      <button className='w-28 py-3 mt-4 bg-black text-white rounded-4xl cursor-pointer'>ADD</button>
     </form>
   )
 }
