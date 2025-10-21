339599694a90aca3e4b37e19af77b6ecbe1ff5ad
rajeev-45 <dixitrajeev5202@gmail.com>
2025-10-21 18:42:06 +0530

    new commit

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -111,7 +111,7 @@ const JobListing = () => {
       {/* Job Listings */}
 
       <section className='w-full lg:w-3/4 text-gray-800 max-lg:px-4'>
-        <h3 className='font-medium text-3xl py-2' id='job-list'>Lstest Jobs</h3>
+        <h3 className='font-medium text-3xl py-2' id='job-list'>Latest Jobs</h3>
         <p className='mb-8'>Get your desired job from top companies</p>
         <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4'>
           {filteredJobs.slice((currentPage-1)*6,currentPage*6).map((job, index) => (
